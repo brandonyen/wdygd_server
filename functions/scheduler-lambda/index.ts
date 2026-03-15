7bd416cd23f49dd410a4be9886f7d050a5db7b6b
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2026-03-15 18:52:41 -0400

    feat: setup EventBridge scheduler, SQS queues and add skeleton lambdas
    yes

---
+++ functions/scheduler-lambda/index.ts
@@ -0,0 +1,4 @@
+exports.handler = async (event: any) => {
+  console.log("Scheduler triggered to check for users needing summaries.");
+  // Logic to determine which users need summaries and send messages to Ingestion Queue
+};
